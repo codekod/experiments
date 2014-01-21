6946a0b9f1e58cfa3764682464afe01366380962
Mattia Pazienti <kuttebo@msn.com>
2014-01-21 16:51:12 +0100

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Place third party dependencies in the lib folder
+//
+// Configure loading modules from the lib directory,
+requirejs.config({
+    "baseUrl": "js",
+    "paths": {
+      "jquery": "jquery.1.8.2.min",
+      "jquery.easing": "jquery.easing.1.3"      
+    },
+    "shim": {
+        "jquery.easing" : { deps:['jquery']},
+        "jquery" : {'exports' : 'jquery'}
+    }
+});
+
+// Load the main app module to start the app
+requirejs(["app/main"]);
